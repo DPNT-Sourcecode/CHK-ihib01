1e297d9e8de444389629fb1922d0733825246e28
sbx_user1051 <sbx_user1051@169.254.42.77>
2022-06-09 10:47:02 +0000

    Thu Jun 09 10:47:02 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -6,12 +6,20 @@ def checkout(skus: str) -> int:
     if skus != str:
         return -1
     else:
-        hash_map = {}
+        hash_map = {
+            "A": {"count": 0, "price": 50},
+            "B": {"count": 0, "price": 30},
+            "C": {"count": 0, "price": 20},
+            "D": {"count": 0, "price": 15}   
+        }
         for sku in skus:
-            if sku not in hash_map:
-                hash_map[sku] = 1
-            else:
-                hash_map[sku] += 1
-                
+            hash_map[sku] += 1
+        total = 0
+        for key in hash_map.keys():
+            total += key["count"] * key["price"]
+        return total
+            
+            
+
 
 
