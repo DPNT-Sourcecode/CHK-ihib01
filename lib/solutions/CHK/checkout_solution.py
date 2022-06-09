f9ee4b66dfa065c65433805b210a16a4ee153ae8
sbx_user1051 <sbx_user1051@169.254.42.77>
2022-06-09 11:46:08 +0000

    Thu Jun 09 11:46:08 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -13,6 +13,15 @@ def checkout(skus: str) -> int:
         num_of_B = list_of_skus.count("B")
         num_of_C = list_of_skus.count("C")
         num_of_D = list_of_skus.count("D")
+        total = 0
+        if num_of_A % 3 == 0:
+            special_price = num_of_A / 3 * 130
+            total += special_price
+        elif num_of_A % 3 == int:
+            special_price = 130 * (num_of_A - (num_of_A % 3))/3
             
             
+            
+            
+
 
