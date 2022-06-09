53978a0375efda777935df50eafb46e0fe535b2d
sbx_user1051 <sbx_user1051@169.254.42.77>
2022-06-09 12:01:08 +0000

    Thu Jun 09 12:01:08 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -21,7 +21,18 @@ def checkout(skus: str) -> int:
         hash_map["B"]["count"] = separated.count("B")
         hash_map["C"]["count"] = separated.count("C")
         hash_map["D"]["count"] = separated.count("D")
+
+        if separated.count("A") % 3 == 0:
+          hash_map["A"]["special_count"] = separated.count("A")/3
+          hash_map["A"]["count"] = 0
+        elif separated.count("A") % 3 == 1 or separated.count("A") % 3 == 2:
+          hash_map["A"]["count"] = separated.count("A") % 3
+          hash_map["A"]["special_count"] = (separated.count("A") - separated.count("A") % 3)/3
+        elif separated.count("B") % 2 == 0:
+            hash_map["B]["special_count"] == separated.count("A")/3
             
+            
+
 
 
 
