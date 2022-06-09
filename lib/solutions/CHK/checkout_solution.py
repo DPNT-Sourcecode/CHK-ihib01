4053076b5608778c4726bca0322417ec0d37318a
sbx_user1051 <sbx_user1051@169.254.42.77>
2022-06-09 10:41:03 +0000

    Thu Jun 09 10:41:03 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,5 +3,15 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus: str) -> int:
-    
+    if skus != str:
+        return -1
+    else:
+        hash_map = {}
+        for sku in skus:
+            if sku not in hash_map:
+                hash_map[sku] = 1
+            else:
+                hash_map[sku] += 1
+                
+
 
