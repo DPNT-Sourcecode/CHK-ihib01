41a9bf27e3ea754dae404302e931c7906d2fe0c8
sbx_user1051 <sbx_user1051@169.254.42.77>
2022-06-09 10:32:09 +0000

    Thu Jun 09 10:32:09 UTC 2022

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -2,5 +2,6 @@
 
 # noinspection PyUnusedLocal
 # friend_name = unicode string
-def hello(friend_name):
-    raise NotImplementedError()
+def hello(friend_name: str) -> str:
+    return f"hello {friend_name}"
+
