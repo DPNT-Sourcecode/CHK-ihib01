62ee3e939a24e31788e8ce9380015db99fb7bf8f
sbx_user1051 <sbx_user1051@169.254.42.77>
2022-06-09 12:07:08 +0000

    Thu Jun 09 12:07:08 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -11,27 +11,33 @@ def checkout(skus: str) -> int:
         separated = list(skus)
         
         hash_map = {
-            "A": {"count": 0, "price": 50, "special_count": 0},
-            "B": {"count": 0, "price": 30, "special_count": 0},
-            "C": {"count": 0, "price": 20},
-            "D": {"count": 0, "price": 15}   
+            "A": {"count": 0, "price": 50, "special_count": 0, "special_price": 130},
+            "B": {"count": 0, "price": 30, "special_count": 0, "special_price": 45},
+            "C": {"count": 0, "price": 20, "special_count": 0, "special_price": 0},
+            "D": {"count": 0, "price": 15, "special_count": 0, "special_price": 0},   
         }
      
         hash_map["A"]["count"] = separated.count("A")
         hash_map["B"]["count"] = separated.count("B")
         hash_map["C"]["count"] = separated.count("C")
         hash_map["D"]["count"] = separated.count("D")
-
+        total = 0
         if separated.count("A") % 3 == 0:
-          hash_map["A"]["special_count"] = int(separated.count("A")/3)
-          hash_map["A"]["count"] = 0
+            hash_map["A"]["special_count"] = int(separated.count("A")/3)
+            hash_map["A"]["count"] = 0
         elif separated.count("A") % 3 == 1 or separated.count("A") % 3 == 2:
-          hash_map["A"]["count"] = separated.count("A") % 3
-          hash_map["A"]["special_count"] = int((separated.count("A") - separated.count("A") % 3)/3)
+            hash_map["A"]["count"] = separated.count("A") % 3
+            hash_map["A"]["special_count"] = int((separated.count("A") - separated.count("A") % 3)/3)
         elif separated.count("B") % 2 == 0:
             hash_map["B"]["special_count"] = int(separated.count("B")/2)
             hash_map["B"]["count"] = 0
         elif separated.count("B") % 2 == 1:
             hash_map["B"]["count"] = separated.count("B") % 2
+            hash_map["B"]["special_count"] = int((separated.count("B") - separated.count("B") % 2)/2)
+            
+        for key, value in hash_map.items():
+            total += 
             
             
+            
+
