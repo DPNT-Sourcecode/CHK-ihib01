bd12677e4bd1ddaeb77768aa52809612432e3383
sbx_user1051 <sbx_user1051@169.254.42.77>
2022-06-09 11:43:08 +0000

    Thu Jun 09 11:43:08 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -8,32 +8,11 @@ def checkout(skus: str) -> int:
     elif type(skus) != str:
         return -1
     else:
-        hash_map = {
-            "A": {"count": 0, "price": 50, "special_count": 0},
-            "B": {"count": 0, "price": 30, "special_count": 0},
-            "C": {"count": 0, "price": 20},
-            "D": {"count": 0, "price": 15}   
-        }
-        for sku in skus:
-            if not sku.isalpha() or not sku.isupper():
-                return -1
-            else:
-                if hash_map[sku] == "A" and hash_map[sku]["count"] == 3:
-                    hash_map[sku]["count"]=0
-                    hash_map[sku]["special_count"] += 1
-                elif hash_map[sku] == "B" and hash_map[sku]["count"] == 2:
-                    hash_map[sku]["special_count"] += 1 
-                    hash_map[sku]["count"] = 0
-                
-                hash_map[sku]["count"] += 1
-        total = 0
-        for key, value in hash_map.items():
-            if key == "A" and value["special_count"] != 0:
-                total += (130 * value["special_count"]) + (value["count"] * value["price"]) 
-            elif key == "B" and value["special_count"] != 0:
-                total += (45 * value["special_count"]) + (value["count"] * value["price"])
-            else:
-                total += value["count"] * value["price"]
-        return total
+        list_of_skus = skus.split('')
+        num_of_A = list_of_skus.count("A")
+        num_of_B = list_of_skus.count("B")
+        num_of_C = list_of_skus.count("C")
+        num_of_D = list_of_skus.count("D")
             
             
+
