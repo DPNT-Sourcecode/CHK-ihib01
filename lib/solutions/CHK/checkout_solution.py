4558db2287a5b0e16f83d56d98e64e98907db1f8
sbx_user1051 <sbx_user1051@169.254.42.77>
2022-06-09 11:19:31 +0000

    Thu Jun 09 11:19:31 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -9,8 +9,8 @@ def checkout(skus: str) -> int:
         return -1
     else:
         hash_map = {
-            "A": {"count": 0, "price": 50},
-            "B": {"count": 0, "price": 30},
+            "A": {"count": 0, "price": 50, "special_count": 0},
+            "B": {"count": 0, "price": 30, "special_count": 0},
             "C": {"count": 0, "price": 20},
             "D": {"count": 0, "price": 15}   
         }
@@ -18,20 +18,19 @@ def checkout(skus: str) -> int:
             if not sku.isalpha() or not sku.isupper():
                 return -1
             else:
+                if hash_map[sku] == "A" and hash_map[sku]["count"] == 3:
+                    hash_map[sku]["special_count"] += 1 and hash_map[sku]["count"] = 0
+                elif hash_map[sku] == "B" and hash_map[sku]["count"] == 2:
+                    hash_map[sku]["special_count"] += 1 and hash_map[sku]["count"] = 0
                 hash_map[sku]["count"] += 1
         total = 0
         for key, value in hash_map.items():
-            if key == "A" and value["count"] % 3 == 0:
-                total += value["count"]/3 * 130
-            elif key == "A" and value["count"] % 3 == int:
-                total += ((value["count"] - (value["count"]%3))/3 * 130) + (value["count"] % 3 * 50)
-            elif key == "B" and value["count"] % 2 == 0:
-                total += value["count"]/2 * 45
-            else:
-                total += value["count"] * value["price"]
+            
+            total += value["count"] * value["price"]
         return total
             
             
+
 
 
 
